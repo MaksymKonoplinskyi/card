a22195d1fbb0ca060ea87b53a947b12c8fab6c53
Your Name <gran7777777@gmail.com>
2021-08-27 20:31:08 +0300

    1

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-// let block = document.getElementById('one');
+
+
+
+let block = document.getElementById('one');
 let keyboard = [1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102, 46];
 
 function init() {
@@ -12,7 +15,7 @@ function init() {
   }
   document.querySelector('#keyboard').innerHTML = out;
 }
-init();
+ init();
 
 document.onkeypress = function (event) {
   console.log(event.code);
@@ -32,3 +35,18 @@ document.querySelectorAll('#keyboard .k-key').forEach(function (element) {
     console.log(code);
   }
 });
+
+
+
+
+$(document).ready(function () {
+  $(".block_title").click(function (event) {
+    // if ($('.block').hasClass('one')) {
+    //   $('block_title').not($(this)).removeClass('active');
+    //   $('block_text').not($(this).next()).slideUp(300);
+    // }
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
+});
+
+console.log($('.block').hasClass('one'));
